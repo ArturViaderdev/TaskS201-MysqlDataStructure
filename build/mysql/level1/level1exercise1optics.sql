8a890be8ab4da090793f949c58a8420a9a1f52bd
ryzenpc <ryzenpc@debianryzen>
2026-03-20 11:16:02 +0100

    fix:fixes and working

--- build/mysql/level1/level1exercise1optics.sql
+++ build/mysql/level1/level1exercise1optics.sql
@@ -85,9 +85,3 @@ INSERT INTO employees(name) VALUES('Juan Costa');
 INSERT INTO sales(glass,client,sold_date,employee,quantity) VALUES(1,1,'2025-01-01',1,1);
 INSERT INTO sales(glass,client,sold_date,employee,quantity) VALUES(1,1,'2026-01-01',1,1);
 INSERT INTO sales(glass,client,sold_date,employee,quantity) VALUES(1,1,'2027-01-01',1,1);
-
-SELECT g.brand,g.model,s.sold_date,s.quantity FROM sales s JOIN glasses g ON s.glass = g.id JOIN clients c ON s.client = c.id WHERE s.sold_date BETWEEN '2025-01-01' AND '2026-01-01' AND c.name="Pedro Reyes";
-
-SELECT DISTINCT b.name,g.model FROM glasses AS g JOIN brands AS b ON g.brand = b.id JOIN sales AS s ON s.glass = g.id JOIN employees AS e ON s.employee = e.id WHERE e.name ='Juan Costa';
-
-SELECT DISTINCT s.name FROM suppliers AS s JOIN brands AS b ON b.supplier = s.id JOIN glasses as g ON g.brand = b.id JOIN sales AS sa ON sa.glass = g.id;
